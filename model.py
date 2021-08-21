0531e59089ef1fd96d6e1ca9673df6b326ee98fa
Long Phung <phungngbalong@gmail.com>
2021-08-22 08:49:11 +0900

    'draft-v2 cho model va train'

--- model.py
+++ model.py
@@ -1,73 +1,65 @@
 # TODO 2:  
-
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from os import name
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.python.keras import activations
-from tensorflow.python.keras.engine.input_layer import Input
-from tensorflow.python.keras.layers.core import Dense, Dropout
-from tensorflow.python.ops.gen_array_ops import shape
 
 class HighwayMLP(tf.keras.Model):
   """
   Highway MLP Layer
   """
-  def __init__(self, input_size, t_bias=-2, acti_h = tf.nn.relu, acti_t = tf.nn.tanh):
+  def __init__(self, input_size, t_bias=-2.0, acti_h = tf.nn.relu, acti_t = tf.nn.tanh):
     super(HighwayMLP, self).__init__()
     self.acti_h = acti_h
     self.acti_t = acti_t
-    self.t_bias = t_bias
+    self.size = input_size
+    self.bias_init = t_bias
 
-    # TODO 
-    self.W = tf.Variable(tf.truncated_normal([input_size, input_size], stddev=0.1), name="weight")
-    self.b = tf.Variable(tf.constant(self.t_bias, shape=[input_size]), name="bias")
-    self.x = None
+    self.W_H = tf.Variable(
+      tf.random.truncated_normal((self.size,self.size), stddev=0.1), name='weight'
+    )
+    self.b_H = tf.Variable(tf.constant(self.bias_init, shape=self.size), name='bias')
 
-    # Dense H
-    self.h = acti_h(tf.matmul(self.x, self.W) + self.b, name="H_gate")
-    # Dense T
-
-    self.t = acti_t(tf.matmul(self.x, self.W) + self.b, name="transform_gate")
-
-
-
-    pass
-
+    self.W_T = tf.Variable(
+      tf.random.truncated_normal((self.size,self.size), stddev=0.1), name='weight'
+    )
+    self.b_T = tf.Variable(tf.constant(self.bias_init, shape=self.size), name='bias')
+  
   def call(self, x):
     # Do Highway: y = H(x,WH)· T(x,WT) + x · C(x,WC).
-    self.x = x
-    y = tf.add(tf.multiply(self.h, self.t), tf.multiply(x, tf.sub(1.0, self.t)), name="highway_layer")
+    # Dense H
+    self.h = self.acti_h(tf.matmul(x, self.W_H) + self.b_H)
+    # Dense T
+    self.t = self.acti_t(tf.matmul(x, self.W_T) + self.b_T)
+    y = tf.add(tf.multiply(self.h,self.t) , tf.multiply(x, (1-self.t)))
 
+    return y
 
-class HighwayNetwork(tf.keras.Model): 
+class HighwayNetwork(tf.keras.Model):
   """
   Highway Network with several layers
   """
   def __init__(self, input_size, output_size):
     super(HighwayNetwork, self).__init__()
-    self.mlplayers = [
-      # TO DO
-      tf.keras.Input(shape=input_size),
-      tf.layers.Dense(71),
-      HighwayMLP.call(),
-      tf.layers.Dense(output_size, activations=tf.nn.softmax)
-    ]
+    self.input_size = input_size
+    self.output_size = output_size
+    self.mlplayers = tf.keras.Sequential([
+      tf.keras.Input(shape=(self.input_size,)),
+      layers.Flatten(),
+      HighwayMLP(input_size),
+      layers.Dense(71, activation="relu"),
+      ])
+    
 
     # Classification layer
+    self.classificationLayer = layers.Dense(self.output_size, activation='softmax')
+    
 
   def call(self, x):
-    # Run input on these mlp layers
-    for layer in self.mlplayers:
-      if layer == 0:
-        prev_y = layer(self.input_size)
-      elif layer == self.mlplayers - 1:
-        y = layer(self.output_size)
-      else:
-        prev_y = layer(self.prev_y)
+    h = self.mlplayers(x)
+    out = self.classificationLayer(h)
 
-    # pass output to classification layer
-
-
-
-model = HighwayNetwork(784, 10)
+    return out
--- train.py
+++ train.py
@@ -1,51 +1,55 @@
+
 import os
 from argparse import ArgumentParser
+
+from tensorflow.python.keras.layers.core import Flatten
 from model import HighwayNetwork
+# from model import HighwayMLP
 
 import tensorflow as tf
-
-def accuracy_function(real, pred):
-    pass
-
-
-def train_step(image, label):
-    pass
+from tensorflow.keras.utils import to_categorical
+# def accuracy_function(real, pred):
+#     pass
 
 
-def val_step():
-    pass
+# def train_step(image, label):
+#     pass
 
 
-@tf.function
-def train():
-    pass
+# def val_step():
+#     pass
+
+
+# @tf.function
+# def train():
+#     pass
     
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
+    # parser = ArgumentParser()
     
-    # FIXME
-    # Arguments users used when running command lines
-    parser.add_argument("--batch-size", default=64, type=int)
-    parser.add_argument("--epochs", default=1000, type=int)
+    # # FIXME
+    # # Arguments users used when running command lines
+    # parser.add_argument("--batch-size", default=64, type=int)
+    # parser.add_argument("--epochs", default=1000, type=int)
 
-    home_dir = os.getcwd()
-    args = parser.parse_args()
+    # home_dir = os.getcwd()
+    # args = parser.parse_args()
 
-    # FIXME
-    # Project Description
+    # # FIXME
+    # # Project Description
 
-    print('---------------------Welcome to ${name}-------------------')
-    print('Github: ${accout}')
-    print('Email: ${email}')
-    print('---------------------------------------------------------------------')
-    print('Training ${name} model with hyper-params:') # FIXME
-    print('===========================')
+    # print('---------------------Welcome to ${name}-------------------')
+    # print('Github: ${accout}')
+    # print('Email: ${email}')
+    # print('---------------------------------------------------------------------')
+    # print('Training ${name} model with hyper-params:') # FIXME
+    # print('===========================')
     
     # FIXME
     # Do Prediction 
 
-    batch_size = 64
+    batch_size = 128
     highway_number = 5
     epochs = 10
     log_interval = 10
@@ -53,8 +57,31 @@ if __name__ == "__main__":
 
     # TODO 1: Load MNIST
 
-
-    # TODO 3: Custom training
+    img_width, img_height = 28, 28
+    # img_size = img_width * img_height
+    number_class = 10
+    mnist = tf.keras.datasets.mnist
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
     
+    x_train = train_images.reshape(60000, 784).astype("float32") / 255
+    x_test = test_images.reshape(10000, 784).astype("float32") / 255
 
+
+    input_size=x_train.shape[1]
+    highway_network = HighwayNetwork(input_size,number_class)
+
+    # Set up loss function
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
+    # Optimizer Definition
+    # All networks were optimized using SGD with momentum # Paper
+    sgd = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.09, nesterov=False, name="SGD")
+    # Compile optimizer and loss function into model
+    highway_network.compile(optimizer='SGD', loss=loss_object, metrics=['acc'])
+
+    highway_network.fit(
+        x_train, train_labels,
+        epochs=epochs,
+        batch_size=batch_size,
+        validation_data=(x_test, test_labels),
+    )
