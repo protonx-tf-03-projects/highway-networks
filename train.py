e5831ee054e92eae69897983cbf8d9c4b5541f9b
pnbl-123 <73574016+pnbl-123@users.noreply.github.com>
2021-09-07 14:29:59 +0900

    Update train.py

--- train.py
+++ train.py
@@ -13,6 +13,8 @@ if __name__ == "__main__":
     parser.add_argument("--num-of-layers", default=3, type=int)
     parser.add_argument("--batch-size", default=32, type=int)
     parser.add_argument("--epochs", default=10, type=int)
+    parser.add_argument("--learning-rate", default=0.01, type=float)
+    parser.add_argument("--model-folder", default='{}/model/highway_network/'.format(home_dir), type=str, required=True)
 
     home_dir = os.getcwd()
     args = parser.parse_args()
@@ -48,7 +50,7 @@ if __name__ == "__main__":
     loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
     # Optimizer Definition
     # All networks were optimized using SGD with momentum # Paper
-    sgd = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.09, nesterov=False, name="SGD")
+    sgd = tf.keras.optimizers.SGD(learning_rate=args.learning_rate, momentum=0.09, nesterov=False, name="SGD")
     # Compile optimizer and loss function into model
     highway_network.compile(optimizer=sgd, loss=loss_object, metrics=['acc'])
 
@@ -59,5 +61,5 @@ if __name__ == "__main__":
         validation_data=(x_test, y_test),
     )
     highway_network.evaluate(x_val, y_val)
-
+    highway_network.save(args.model_folder)
     # highway_network.summary()
