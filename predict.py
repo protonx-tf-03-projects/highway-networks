86352bac69a73fd9996aa74239b2a10c52f3a771
pnbl-123 <73574016+pnbl-123@users.noreply.github.com>
2021-09-09 18:00:12 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,24 +1,25 @@
 import os
 from argparse import ArgumentParser
+from model import HighwayNetwork
+import numpy as np
+import tensorflow as tf
+from data import build_dataset
+
 
 if __name__ == "__main__":
+    home_dir = os.getcwd()
     parser = ArgumentParser()
-    parser.add_argument("--batch-size", default=64, type=int)
-    parser.add_argument("--epochs", default=1000, type=int)
+    parser.add_argument("--model-folder", default='{}/model/highway/'.format(home_dir), type=str)
+    parser.add_argument("--image-size", default=28, type=int)
+    parser.add_argument("--image-index", default=0, type=int)
 
-    # FIXME
     args = parser.parse_args()
+    # Loading Model
+    highway = tf.keras.models.load_model(args.model_folder)
+    mnist = build_dataset()
+    _, _, _, _, x_val, y_val= mnist
+    predictions = highway.predict(x_val)  
 
-    # FIXME
-    # Project Description
-
-    print('---------------------Welcome to ${name}-------------------')
-    print('Github: ${accout}')
-    print('Email: ${email}')
-    print('---------------------------------------------------------------------')
-    print('Training ${name} model with hyper-params:') # FIXME
-    print('===========================')
-
-    # FIXME
-    # Do Training
-
+    print('---------------------Prediction Result: -------------------')
+    print('Output Softmax: {}'.format(np.argmax(predictions[args.image_index]), axis=1))
+    print('This image belongs to class: {}'.format(np.argmax(y_val[args.image_index]), axis=1))
