ad36a87315fe109d18956e4adcc3a1afacbad8e8
pnbl-123 <73574016+pnbl-123@users.noreply.github.com>
2021-09-09 18:02:48 +0900

    Update data.py

--- data.py
+++ data.py
@@ -1,9 +1,19 @@
-class Dataset:
-    def __init__(self):
-        pass
+import tensorflow as tf
 
-    def load_dataset(self):
-        pass
+def build_dataset():
+    # load data mnist
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    datasets = []
+    # Normalize data
+    x_train = x_train.reshape(-1, 784).astype("float32") / 255
+    x_test = x_test.reshape(-1, 784).astype("float32") / 255
+    train_num = len(x_train)
 
-    def build_dataset(self):
-        pass
+    # Reserve 10,000 samples for validation
+    x_val = x_train[-10000:]
+    y_val = y_train[-10000:]
+    x_train = x_train[:-10000]
+    y_train = y_train[:-10000]
+
+    return x_train, y_train, x_test, y_test, x_val, y_val
